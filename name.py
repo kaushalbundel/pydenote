1cc8631caec0f2f420f79db6f995ffbf470594c1
Kaushal Bundel <kaushalbundel@gmail.com>
2025-02-22 17:07:01 +0000

    formatting updated

--- name.py
+++ name.py
@@ -1,13 +1,13 @@
-'''
-The file contains code that will be used to generate the name of the file.
-'''
+##########################################################################
+# The file contains code that will be used to generate the name of the file.
+##########################################################################
 
 from datetime import datetime
 from typing import Tuple
 
 def get_date_time() -> Tuple[str, str]:
     '''
-    Function extracts date and time by using inbuilt datetime module.
+    Function extracts date and time in string format by using inbuilt datetime module.
     '''
     formatted_date: str = datetime.today().strftime('%Y%m%d %H%M%S')
     return tuple(formatted_date.split(" "))
