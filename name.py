374444a1648356c970dbb01529fb65c63b36f9c7
Kaushal Bundel <kaushalbundel@gmail.com>
2025-02-22 17:05:20 +0000

    function name changed

--- name.py
+++ name.py
@@ -5,11 +5,9 @@ The file contains code that will be used to generate the name of the file.
 from datetime import datetime
 from typing import Tuple
 
-def extract_date() -> Tuple[str, str]:
+def get_date_time() -> Tuple[str, str]:
     '''
     Function extracts date and time by using inbuilt datetime module.
     '''
     formatted_date: str = datetime.today().strftime('%Y%m%d %H%M%S')
     return tuple(formatted_date.split(" "))
-
-print(extract_date())
